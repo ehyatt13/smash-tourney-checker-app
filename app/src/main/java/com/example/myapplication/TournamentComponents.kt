944be786cd14a26948bf3fddf92ad1617e123d03
ehyatt13 <ethan.hyatt13@gmail.com>
2026-07-25 14:11:29 -0400

    commented unused imports

--- app/src/main/java/com/example/myapplication/TournamentComponents.kt
+++ app/src/main/java/com/example/myapplication/TournamentComponents.kt
@@ -1,8 +1,8 @@
 package com.example.myapplication
 
 import android.content.res.Configuration
-import android.graphics.Color
-import android.view.View
+//import android.graphics.Color
+//import android.view.View
 import android.widget.CalendarView
 import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.*
@@ -13,7 +13,7 @@ import androidx.compose.runtime.*
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.platform.LocalConfiguration
-import androidx.compose.ui.platform.LocalContext
+//import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.text.font.FontStyle
